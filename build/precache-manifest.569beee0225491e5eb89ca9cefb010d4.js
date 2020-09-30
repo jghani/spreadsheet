3ed0a2ed83a7183981486592e1b42b3eebadfc1b
jghani <jibranghani1@gmail.com>
2020-09-30 16:48:53 +0500

    master

---
+++ build/precache-manifest.569beee0225491e5eb89ca9cefb010d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd5a28d89683d4a1c8004309827fe66a",
+    "url": "/spreadsheet/index.html"
+  },
+  {
+    "revision": "01e89cee0d705ecc8ee8",
+    "url": "/spreadsheet/static/css/main.215b8cab.chunk.css"
+  },
+  {
+    "revision": "f94ecfaacac7f8eb3ad5",
+    "url": "/spreadsheet/static/js/2.4c4a4af2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spreadsheet/static/js/2.4c4a4af2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01e89cee0d705ecc8ee8",
+    "url": "/spreadsheet/static/js/main.6d702ddc.chunk.js"
+  },
+  {
+    "revision": "d21e632b229d136077bf",
+    "url": "/spreadsheet/static/js/runtime-main.f2f92bae.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import SpreadSheet from "./Components/SpreadSheet/SpreadSheet";
+
+function App() {
+  return (
+    <div style={{ width: 'max-content' }}>
+      <SpreadSheet x={26} y={50} />
+  </div>
+  );
+}
+
+export default App;
